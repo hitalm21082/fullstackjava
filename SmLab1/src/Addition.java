6a34bb55d17f933eb1f44060fb6b83aaecec4038
hitalm21082 <87126075+hitalm21082@users.noreply.github.com>
2021-07-09 23:12:51 +0530

    Add files via upload

---
+++ SmLab1/src/Addition.java
@@ -0,0 +1,12 @@
+
+public class Addition {
+	public static void main(String[] args) {
+		System.out.println("enter two numbers:");
+		int i=100;
+		int j=200;
+		System.out.println(i+" "+j);
+		int sum=i+j;
+		System.out.println("The sum is: "+sum);
+}
+	}
+
---
+++ SmLab1/src/TypeCasting1.java
@@ -0,0 +1,12 @@
+
+public class TypeCasting1 {
+public static void main(String[]args){
+	byte b=10;
+	int i=b;
+	byte c=(byte)i;
+	byte d=(byte)i;
+	byte x=10;
+	byte y=20;
+	byte sum=(byte)(x*y);
+	}
+}
