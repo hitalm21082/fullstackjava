df2463210fc20ae7a6f4d19f03f547f7a5cb3475
hitalm21082 <87126075+hitalm21082@users.noreply.github.com>
2021-07-23 12:29:01 +0530

    Add files via upload

---
+++ almamatter/lab8practice/EscapeDemo.java
@@ -0,0 +1,27 @@
+package lab8practice;
+
+public class EscapeDemo {
+		public static void main(String[] args) {
+			System.out.println("hello\thai\tworld");
+			
+			
+			System.out.println("jack and jill went \"up\" the hill");
+			System.out.println("jack and jill went \'up\' the hill");
+			System.out.println("jack and jill went \\up\\ the hill");
+			
+			System.out.println("jack and jill went \n up \n the hill");
+			
+			System.out.println("jack and jill went \f up \f the hill");//form feed-page break
+			
+			System.out.println("hello\b\b\bW");
+			int i=0;
+			while(i<1000000) {
+				i++;
+				System.out.print(i+"\r");//carriage return
+			}
+			
+			
+			
+		}
+
+	}
---
+++ almamatter/lab9practice/CommandlineDemo.java
@@ -0,0 +1,8 @@
+package lab9practice;
+
+public class CommandlineDemo {
+	public static void main(String[] args) {
+		System.out.println("Welcome to JAVA...:"+args[0]+"..."+args[1]);
+
+}
+}
